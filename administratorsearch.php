5248fcf2216db749b519819b866d3c3a40a75410
k4n4 <92017520+k4n4@users.noreply.github.com>
2022-02-23 22:51:21 +0900

    20220223

--- administratorsearch.php
+++ administratorsearch.php
@@ -21,11 +21,19 @@ $empno = $_POST['empno'];
 // echo $empno;
 
 $view = '';
+$view2 = '';
 $workdate = array();
 $starttime = array();
 $endtime = array();
 $place = array();
-$where = 'WHERE ';
+
+// 入力チェックはadministratorconfirm.phpで行う(required)
+
+// if($empno == 0) {
+//   $alert = "<script type='text/javascript'>alert('日付と社員番号は両方入力してください');</script>";
+//   echo $alert;
+//   exit();
+// }
 
 //以下検索機能
 //1.DB接続
@@ -92,24 +100,77 @@ if($status==false) {
     $view .= '</p>';
 
     // WHERE句を作成
+    $where = ' WHERE NOT (transaction.empno = "'.$empno.'") and (';
     $j = 0;
     for($i = 0 ; $i < count($starttime); $i++){
      $where .= '(transaction.workdate = "'.$workdate[$i]. '" and ';
      $where .= '(transaction.starttime <= "'.$endtime[$i].'" or ';
-     $where .= 'transaction.endtime >= "'.$starttime[$i].'" ) ';
+     $where .= 'transaction.endtime <= "'.$starttime[$i].'" ) ';
      $where .= 'and workplace_mst.workplacename = "'.$place[$i].'" ) ';
      if ($j < count($starttime)-1) {
         $where .= ' or ';
      }
      $j = $j +1;
     }
-    
+    $where .= ')';
 
     // var_dump($workdate);
     // var_dump($starttime);
     // var_dump($endtime);
     // var_dump($place);
-    echo $where;
+    // echo $where;
+
+    // 接触者検索のSQL作成
+    $stmt = $pdo->prepare("SELECT transaction.recordID,transaction.workdate,transaction.starttime,transaction.endtime,transaction.empno,employee_mst.empname,department_mst.departmentname,workplace_mst.workplacename,transaction.remarks
+    FROM
+    transaction
+    JOIN
+    employee_mst
+    ON
+    transaction.empno = employee_mst.empno
+    JOIN
+    department_mst
+    ON
+    employee_mst.departmentno = department_mst.departmentno 
+    JOIN
+    workplace_mst
+    ON
+    transaction.workplaceno = workplace_mst.workplaceno ".$where);
+    $status = $stmt->execute();
+
+    if($status==false) {
+        sql_error($stmt);
+      }else{
+        $view2 .= '<p>';
+        $view2 .= '<TABLE border="1" width="1000" style="font-size: 10pt">';
+        //タイトル表示
+        $view2 .= '<tr bgcolor="#FFDBC9">';
+        $view2 .= '<th width="50">No.</th>';
+        $view2 .= '<th width="200">出勤日</th>';
+        $view2 .= '<th width="200">出勤時間</th>';
+        $view2 .= '<th width="200">退勤時間</th>';
+        $view2 .= '<th width="200">社員番号</th>';
+        $view2 .= '<th width="250">社員名</th>';
+        $view2 .= '<th width="300">所属</th>';
+        $view2 .= '<th width="250">出社先</th>';
+        $view2 .= '<th width="400">備考</th>';
+        $view2 .= '</tr>';
+        while( $r = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+          $view2 .= '<tr>';
+          $view2 .= '<th>'.$r["recordID"].'</th>';
+          $view2 .= '<th>'.$r["workdate"].'</th>';
+          $view2 .= '<th>'.$r["starttime"].'</th>';
+          $view2 .= '<th>'.$r["endtime"].'</th>';
+          $view2 .= '<th>'.$r["empno"].'</th>';
+          $view2 .= '<th>'.$r["empname"].'</th>';
+          $view2 .= '<th>'.$r["departmentname"].'</th>';
+          $view2 .= '<th>'.$r["workplacename"].'</th>';
+          $view2 .= '<th>'.$r["remarks"].'</th>';
+          $view2 .= '</tr>';
+        }
+      }
+        $view2 .= '</TABLE>';
+        $view2 .= '</p>';
 ?>
 
 
@@ -133,15 +194,12 @@ if($status==false) {
 
 <!-- Main[Start] -->
 <form method="POST" action="administratorsearch.php">
-    <fieldset>
-    <legend>◆該当人物の行動履歴の検索◆</legend><br>
-     <label>日付<input type="date" name="date" maxlength="4" size="4"></label><br>
-     <label>社員番号：<input type="" name="empno"></label><br>
-     <!-- <label>郵便番号：<input type="text" name="postcode" maxlength="7" size="7"></label><br>
-     <label>住所：<input type="text" name="address" maxlength="40" size="40"></label><br>
-     <br> -->
-     <input type="submit" value="検索">
-    </fieldset>
+  <fieldset>
+  <legend>◆該当人物の行動履歴の検索◆</legend><br>
+    <label>日付<input type="date" name="date" maxlength="4" size="4" required></label><br>
+    <label>社員番号：<input type="" name="empno" required></label><br>
+    <input type="submit" value="検索">
+  </fieldset>
 </form>
 <br>
 <legend>◆該当人物の行動履歴を表示します◆</legend>
@@ -155,6 +213,13 @@ if($status==false) {
     <?= $view2 ?>
     
 </div>
+
+<!-- エクセル出力のボタン -->
+<form method="POST" action="fileexport.php">
+  <!-- <input type="hidden" name="view" value="<?=$view ?>"> -->
+  <input type="submit" value="接触者をファイルに出力する">
+</form>
+
 <!-- Main[End] -->
 
 </body>
